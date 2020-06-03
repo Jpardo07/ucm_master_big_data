1678953929f9efe379cbd9e7c27a18211d584d96
PedroConcejero <pedro.concejerocerezo@gmail.com>
2020-06-03 23:41:27 +0200

    subo contenido master ucm

---
+++ ui.R
@@ -0,0 +1,58 @@
+#
+# Ejemplo de app shiny para master UNED Big Data
+# Módulo visualización avanzada
+#
+# Ejemplo de ejercicio con datos COVID por edad
+
+library(shiny)
+library(ggplot2)
+library(tidyverse)
+
+# Cargamos los datos desde el repositorio github datadista 
+
+url <- "https://raw.githubusercontent.com/datadista/datasets/master/COVID%2019/nacional_covid19_rango_edad.csv"
+
+dataset <- read_csv(url)
+
+  names(dataset)[1] <- "fecha"
+
+fecha <- max(dataset$fecha)
+
+cond <- {dataset$fecha == fecha & 
+    dataset$rango_edad != "Total"}
+
+data_para_plot <- dataset[cond, ]
+
+# distinguimos variables "a nivel de intervalo" ("continuas" para ggplot)
+nums <- sapply(data_para_plot, is.numeric)
+continuas <- names(data_para_plot)[nums]
+
+# y variables "categóricas" ("discretas" para ggplot)
+cats <- sapply(data_para_plot, is.character)
+categoricas <- names(data_para_plot)[cats]
+
+
+shinyUI(
+  navbarPage("Shiny Visualización Avanzada",
+                   tabPanel("Descripción del trabajo",
+                            mainPanel(
+                              h1("Ejercicio Visualización Avanzada", align = "center"),
+                              h2("Propuesto por Pedro Concejero", align = "center"),
+                              p("Ejemplo de gráfico COVID-19 por grupos de edad"),
+                              p("En función de si consideramos hospitalizados, UCI y fallecidos")
+                              )),
+                   tabPanel("Barplot",
+                            sidebarPanel(
+                              
+                              selectInput('y', 'Elige variable para eje Y', continuas, continuas[[4]]),
+                            ),
+                            
+                            mainPanel(
+                              plotOutput('plot',
+                                         height=500)
+                              
+                            )
+                   )
+  
+))
+
